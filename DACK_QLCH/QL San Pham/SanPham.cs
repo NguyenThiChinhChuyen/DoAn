76bc800346091b2a326ae6fa88ebd733fd412de9
KhanhToan-HT <65375740+KhanhToan-HT@users.noreply.github.com>
2020-07-08 11:55:43 +0700

    bo sung from

---
+++ DACK_QLCH/QL San Pham/SanPham.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DACK_QLCH.QL_San_Pham
+{
+    public partial class SanPham : Form
+    {
+        public SanPham()
+        {
+            InitializeComponent();
+        }
+    }
+}
